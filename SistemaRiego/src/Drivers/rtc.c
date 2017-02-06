b5b4e2fa0d955022c197d0a32742f4e560171294
Martin Maccio <martinalemaccio_89@hotmail.com>
2017-02-06 00:24:54 -0300

    Se cambiaron incializaciones

--- SistemaRiego/src/Drivers/rtc.c
+++ SistemaRiego/src/Drivers/rtc.c
@@ -7,10 +7,9 @@
 */
 #include "infotronic.h"
 
-void init_rtc(uint8_t ss, uint8_t mm, uint8_t hh, uint8_t dd, uint8_t mo, uint16_t yy)
+void init_rtc(void)
 {
 	RTCCCR = 0x01UL; //Se habilita clock
-	config_date_RTC(ss,mm,hh,dd,mo,yy); //Se configura fecha y hora del reloj
 	RTCCIIR &= ~(0xFFUL); //No se generan interrupciones
 	RTCARM |= 0xFFUL; //Inicialmente no se utiliza la alarma
 	//ISER0  |= (1 << 17); //Se habilita interrupcion del RTC
--- SistemaRiego/src/Headers/rtc.h
+++ SistemaRiego/src/Headers/rtc.h
@@ -60,7 +60,7 @@
 
 #define PCRTC 9
 
-void init_rtc(uint8_t ss, uint8_t mm, uint8_t hh, uint8_t dd, uint8_t mo, uint16_t yy);
+void init_rtc(void);
 
 void config_date_RTC(uint8_t ss, uint8_t mm, uint8_t hh, uint8_t dd, uint8_t mo, uint16_t yy);
 
